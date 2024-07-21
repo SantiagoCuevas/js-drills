6d14bce40743b9839ee94ecc82c4f307b7c65415
Santiago Cuevas <santiagocuevas77@gmail.com>
2024-07-21 15:21:01 -0600

    set up editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
+
+const extensions = [javascript()];
+
 function App() {
   return (
     <>
       <h1>JS Drills</h1>
+      <CodeMirror
+        value="console.log('Hello World')"
+        height="200px"
+        theme={tokyoNight}
+        extensions={extensions}
+      />
     </>
   );
 }
